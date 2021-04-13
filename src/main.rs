0486544a69a68b7921c5a433f3a47cd5d3ee0432
Julia <juliariety@gmail.com>
2021-04-13 21:39:49 +0200

    Add Vec3, Point3, Color types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod vec3;
+
 fn main() {
     // Image
 
---
+++ src/vec3.rs
@@ -0,0 +1,5 @@
+use nalgebra::Vector3;
+
+pub(crate) type Vec3 = Vector3<f64>;
+pub(crate) type Point3 = Vec3;
+pub(crate) type Color = Vec3;
