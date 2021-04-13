a6af0c6b056e13c545420a1b7cc3ba4ea5bf9964
Julia <juliariety@gmail.com>
2021-04-13 23:13:15 +0200

    Generate a blue to white gradient

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,36 @@
 use color::write_color;
-use vec3::Color;
+use ray::Ray;
+use vec3::{unit_vector, Color, Point3, Vec3};
 
 mod color;
 mod ray;
 mod vec3;
 
+fn ray_color(ray: &Ray) -> Color {
+    let unit_direction = unit_vector(ray.direction());
+    let t = 0.5 * (unit_direction.y + 1.0);
+
+    (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
+}
+
 fn main() {
     // Image
 
-    const IMAGE_WIDTH: usize = 256;
-    const IMAGE_HEIGHT: usize = 256;
+    const ASPECT_RATIO: f64 = 16.0 / 9.0;
+    const IMAGE_WIDTH: usize = 400;
+    const IMAGE_HEIGHT: usize = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as usize;
+
+    // Camera
+
+    const VIEWPORT_HEIGHT: f64 = 2.0;
+    const VIEWPORT_WIDTH: f64 = ASPECT_RATIO * VIEWPORT_HEIGHT;
+    const FOCAL_LENGTH: f64 = 1.0;
+
+    let origin = Point3::new(0.0, 0.0, 0.0);
+    let horizontal = Vec3::new(VIEWPORT_WIDTH, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, VIEWPORT_HEIGHT, 0.0);
+    let lower_left_corner =
+        origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, FOCAL_LENGTH);
 
     // Render
 
@@ -19,11 +40,13 @@ fn main() {
         eprint!(".");
 
         for i in 0..IMAGE_WIDTH {
-            let pixel_color = Color::new(
-                i as f64 / (IMAGE_WIDTH - 1) as f64,
-                j as f64 / (IMAGE_HEIGHT - 1) as f64,
-                0.25,
+            let u = i as f64 / (IMAGE_WIDTH - 1) as f64;
+            let v = j as f64 / (IMAGE_HEIGHT - 1) as f64;
+            let r = Ray::new(
+                origin,
+                lower_left_corner + u * horizontal + v * vertical - origin,
             );
+            let pixel_color = ray_color(&r);
 
             write_color(&pixel_color);
         }
--- src/vec3.rs
+++ src/vec3.rs
@@ -3,3 +3,15 @@ use nalgebra::Vector3;
 pub(crate) type Vec3 = Vector3<f64>;
 pub(crate) type Point3 = Vec3;
 pub(crate) type Color = Vec3;
+
+pub(crate) fn length_squared(v: &Vec3) -> f64 {
+    v.x * v.x + v.y * v.y + v.z * v.z
+}
+
+pub(crate) fn length(v: &Vec3) -> f64 {
+    length_squared(v).sqrt()
+}
+
+pub(crate) fn unit_vector(v: &Vec3) -> Vec3 {
+    v / length(v)
+}
