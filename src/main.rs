f5b73de5263df9d421926e9044ea5a9ae972ea51
Julia <juliariety@gmail.com>
2021-04-13 21:33:15 +0200

    Add a progress indicator

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ fn main() {
     println!("P3\n{} {}\n255", IMAGE_WIDTH, IMAGE_HEIGHT);
 
     for j in (0..IMAGE_HEIGHT).rev() {
+        eprint!(".");
+
         for i in 0..IMAGE_WIDTH {
             let r = i as f64 / (IMAGE_WIDTH - 1) as f64;
             let g = j as f64 / (IMAGE_HEIGHT - 1) as f64;
