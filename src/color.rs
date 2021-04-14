e0d9263ecf42297ab96220d51033715c23510d63
Julia <juliariety@gmail.com>
2021-04-14 19:29:40 +0200

    Apply gamma correction

--- src/color.rs
+++ src/color.rs
@@ -2,14 +2,15 @@ use crate::rtweekend::clamp;
 use crate::vec3::Color;
 
 pub(crate) fn write_color(pixel_color: &Color, samples_per_pixel: usize) {
-    let mut r = pixel_color.x;
-    let mut g = pixel_color.y;
-    let mut b = pixel_color.z;
+    let r = pixel_color.x;
+    let g = pixel_color.y;
+    let b = pixel_color.z;
 
+    // Divide the color by the number of samples and gamma-correct for gamma=2.0.
     let scale = 1.0 / samples_per_pixel as f64;
-    r *= scale;
-    g *= scale;
-    b *= scale;
+    let r = (scale * r).sqrt();
+    let g = (scale * g).sqrt();
+    let b = (scale * b).sqrt();
 
     println!(
         "{} {} {}",
