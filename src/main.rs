0b7584ad88dc35ef48933a878e1ee03242603d3c
Julia <juliariety@gmail.com>
2021-05-02 14:54:04 +0200

    Don't hardcode any arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use argh::FromArgs;
 use color_eyre::eyre::Result;
 use image::ColorType;
 use indicatif::{ProgressBar, ProgressStyle};
@@ -20,6 +21,46 @@ mod rtweekend;
 mod scene_loader;
 mod sphere;
 mod vec3;
+
+/// A ray tracer.
+#[derive(FromArgs)]
+struct Args {
+    /// scene file
+    #[argh(option, short = 'f', default = "\"scene.dhall\".to_string()")]
+    scene_file: String,
+
+    /// output
+    #[argh(option, short = 'o', default = "\"image.png\".to_string()")]
+    output: String,
+
+    /// aspect ratio
+    #[argh(option, short = 'a', default = "16.0 / 9.0")]
+    aspect_ratio: f64,
+
+    /// image width
+    #[argh(option, short = 'w', default = "1200")]
+    image_width: u32,
+
+    /// samples per pixel
+    #[argh(option, short = 's', default = "10")]
+    samples_per_pixel: usize,
+
+    /// max depth
+    #[argh(option, short = 'd', default = "50")]
+    max_depth: usize,
+
+    /// focus distance
+    #[argh(option, short = 'D', default = "10.0")]
+    focus_distance: f64,
+
+    /// aperture
+    #[argh(option, short = 'A', default = "0.1")]
+    aperture: f64,
+
+    /// vertical field-of-view
+    #[argh(option, short = 'v', default = "20.0")]
+    vfov: f64,
+}
 
 fn ray_color(r: &Ray, world: &dyn Hittable, depth: usize) -> Color {
     // If we've exceeded the ray bounce limit, no more light is gathered.
@@ -49,13 +90,16 @@ fn ray_color(r: &Ray, world: &dyn Hittable, depth: usize) -> Color {
 
 fn main() -> Result<()> {
     color_eyre::install()?;
+
+    let args: Args = argh::from_env();
+
     // Image
 
-    const ASPECT_RATIO: f64 = 16.0 / 9.0;
-    const IMAGE_WIDTH: u32 = 1200;
-    const IMAGE_HEIGHT: u32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as u32;
-    const SAMPLES_PER_PIXEL: usize = 50;
-    const MAX_DEPTH: usize = 50;
+    let aspect_ratio = args.aspect_ratio;
+    let image_width = args.image_width;
+    let image_height: u32 = (image_width as f64 / aspect_ratio) as u32;
+    let samples_per_pixel = args.samples_per_pixel;
+    let max_depth = args.max_depth;
 
     // World
 
@@ -64,11 +108,11 @@ fn main() -> Result<()> {
     const PROGRESS_BAR_TEMPLATE_WITHOUT_MESSAGE: &'static str =
         "[{elapsed_precise}] ({eta_precise}) [{wide_bar}]";
 
-    let pb = ProgressBar::new(IMAGE_HEIGHT as u64);
+    let pb = ProgressBar::new(image_height as u64);
     pb.set_style(ProgressStyle::default_bar().template(PROGRESS_BAR_TEMPLATE));
 
     pb.set_message("loading scene");
-    let world = load_scene("scene.dhall")?;
+    let world = load_scene(&args.scene_file)?;
 
     // You'd think we'd just be able to call pb.set_message(""), but that
     // leaves a gap where the message is supposed to go.
@@ -82,38 +126,38 @@ fn main() -> Result<()> {
     let look_from = Point3::new(13.0, 2.0, 3.0);
     let look_at = Point3::new(0.0, 0.0, 0.0);
     let vup = Vec3::new(0.0, 1.0, 0.0);
-    const DIST_TO_FOCUS: f64 = 10.0;
-    const APERTURE: f64 = 0.1;
+    let dist_to_focus = args.focus_distance;
+    let aperture = args.aperture;
 
     let camera = Camera::new(
         look_from,
         look_at,
         vup,
-        20.0,
-        ASPECT_RATIO,
-        APERTURE,
-        DIST_TO_FOCUS,
+        args.vfov,
+        aspect_ratio,
+        aperture,
+        dist_to_focus,
     );
 
     // Render
-    let mut image_data = Vec::with_capacity((IMAGE_WIDTH * IMAGE_HEIGHT * 3) as usize);
+    let mut image_data = Vec::with_capacity((image_width * image_height * 3) as usize);
 
-    for j in (0..IMAGE_HEIGHT).rev() {
+    for j in (0..image_height).rev() {
         pb.inc(1);
 
-        for i in 0..IMAGE_WIDTH {
-            let pixel_color = (0..SAMPLES_PER_PIXEL)
+        for i in 0..image_width {
+            let pixel_color = (0..samples_per_pixel)
                 .map(|_| {
-                    let u = (i as f64 + random_double()) / (IMAGE_WIDTH - 1) as f64;
-                    let v = (j as f64 + random_double()) / (IMAGE_HEIGHT - 1) as f64;
+                    let u = (i as f64 + random_double()) / (image_width - 1) as f64;
+                    let v = (j as f64 + random_double()) / (image_height - 1) as f64;
 
                     camera.get_ray(u, v)
                 })
                 .fold(Color::new(0.0, 0.0, 0.0), |pixel_color, r| {
-                    pixel_color + ray_color(&r, &world, MAX_DEPTH)
+                    pixel_color + ray_color(&r, &world, max_depth)
                 });
 
-            let (r, g, b) = clamp_color(&pixel_color, SAMPLES_PER_PIXEL);
+            let (r, g, b) = clamp_color(&pixel_color, samples_per_pixel);
 
             image_data.push(r);
             image_data.push(g);
@@ -122,10 +166,10 @@ fn main() -> Result<()> {
     }
 
     image::save_buffer(
-        "image.png",
+        args.output,
         &image_data,
-        IMAGE_WIDTH,
-        IMAGE_HEIGHT,
+        image_width,
+        image_height,
         ColorType::Rgb8,
     )?;
 
